599705f5e4e1c0d32112351d0a12ec2d42f94815
StevinRAD <sstepin3rd@gmail.com>
2026-04-24 12:02:36 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // Gunakan gambar lokal saja untuk performa lebih baik
-    unoptimized: true, // Disable image optimization di dev untuk reload lebih cepat
-    qualities: [100, 75], // Tambahkan quality 100 untuk gambar berkualitas tinggi
+    unoptimized: true,
+    qualities: [100, 75],
+  },
+  experimental: {
+    turbo: {},
   },
 };
 
